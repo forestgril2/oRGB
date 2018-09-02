21a81a71dd22f3d848382689c4043a0e9c58d9f1
forestgril2 <forestgril@yahoo.com>
2018-09-02 10:55:49 +0200

    Transforming forth to LCC and back to RGB but with artifacts (lacks correct gamut mapping?)

--- TransformORGB.cpp
+++ TransformORGB.cpp
@@ -2,50 +2,61 @@
 #include <QDebug>
 #include <QImage>
 #include <QVector3D>
+#include <cmath>
 
 TransformORGB::TransformORGB(QObject *parent) : QObject(parent)
 {
+}
 
-    float toLCCvals[]   = {0.299,  0.587, 0.114,
-                              0.5,    0.5,  -1.0,
-                            0.866, -0.866,   0.0};
+void TransformORGB::transformPixels(QImage& image, QMatrix4x4 toLCC)
+{
+    for (int i = 0; i< image.width(); ++i)
+    {//transorm to LCC
+       for (int j = 0; j< image.height(); ++j)
+       {
+          QRgb pixel = image.pixel(i, j);
+          QVector3D pixelRGB_vals = {static_cast<float>(qRed(pixel)),
+                                     static_cast<float>(qGreen(pixel)),
+                                     static_cast<float>(qBlue(pixel))};
 
-    float fromLCCvals[] = {1.0,  0.114,  0.7436,
-                            1.0,  0.114, -0.4111,
-                            1.0, -0.866,  0.1663};
+          QVector3D pixelLCC_vals = QVector3D(toLCC*(pixelRGB_vals.toVector4D()));
+          QRgb pixelLCC = qRgb(static_cast<int>(std::round(pixelLCC_vals.x())),
+                               static_cast<int>(std::round(pixelLCC_vals.y())),
+                               static_cast<int>(std::round(pixelLCC_vals.z())));
 
-//    toLCC =   QGenericMatrix<3,3,float>(toLCCvals);
-//    fromLCC = QGenericMatrix<3,3,float>(fromLCCvals);
+          image.setPixel(i, j, pixelLCC);
+       }
+    }
 }
 
 void TransformORGB::transform(QString filePath)
 {
     qDebug() << " ### file: " << filePath << " is being transformed";
 
+    QString truncatedPath = filePath;
+    truncatedPath.replace("file:///", "");
+
     QImage image;
-    image.load(filePath);
-
-    for (int i = 0; i< image.width(); ++i)
+    if (!image.load(truncatedPath))
     {
-       for (int j = 0; j< image.height(); ++j)
-       {
-          QRgb pixel = image.pixel(i, j);
-
-          QVector3D pixelRGB_vals = {qRed(pixel), qGreen(pixel), qBlue(pixel)};
-          QVector3D pixelLCC_vals = QVector3D(toLCC*(pixelRGB_vals.toVector4D()));
-
-          QRgb pixelLCC = qRgb(pixelLCC_vals.x(), pixelLCC_vals.y(), pixelLCC_vals.z());
-          image.setPixel(i, j, pixelLCC);
-       }
+        qDebug() << " ### Error loading file: " << truncatedPath;
     }
 
-    int dotPosition = filePath.indexOf(".");
-    QString transformedPath =  filePath.insert(dotPosition, "_transformed");
+//    qDebug() << " ### Image format: " << image.format(); //Format_RGB32
+
+    transformPixels(image, toLCC);
+    transformPixels(image, fromLCC);
+
+    QString transformedPath = truncatedPath;
+    int dotPosition = truncatedPath.indexOf(".");
+
+    transformedPath.insert(dotPosition, "_transformed");
     QImage imageLCC(image);
-    if (!imageLCC.save(transformedPath.replace("file://", ""), "PNG"))
+    if (!imageLCC.save(transformedPath))
     {
         qDebug() << " ### Error saving file: " << transformedPath;
     }
+
 
     emit fileReady(transformedPath);
 }
--- TransformORGB.h
+++ TransformORGB.h
@@ -15,12 +15,17 @@ signals:
     void fileReady(QString filePath);
 
 private:
-    const QMatrix4x4 toLCC = {0.299,  0.587, 0.114, 0,
-                                0.5,    0.5,  -1.0, 0,
-                              0.866, -0.866,   0.0, 0,
-                                  0,      0,     0, 0};
-    QMatrix4x4 fromLCC;
+    const QMatrix4x4 toLCC = {0.299,  0.587,   0.114, 0,
+                                0.5,    0.5,    -1.0, 0,
+                              0.866, -0.866,     0.0, 0,
+                                  0,      0,       0, 1};
 
+    QMatrix4x4 fromLCC     = {  1.0,  0.114,  0.7436, 0,
+                                1.0,  0.114, -0.4111, 0,
+                                1.0, -0.866,  0.1663, 0,
+                                  0,      0,       0, 1};
+
+    void transformPixels(QImage &image, QMatrix4x4 toLCC);
 };
 
 #endif // TRANSFORMORGB_H
